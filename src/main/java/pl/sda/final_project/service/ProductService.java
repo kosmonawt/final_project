bf2c43ddf9129111445bd6d1e407622efc1c036b
Konstantyn <kosvkv@gmail.com>
2020-09-05 16:05:20 +0200

    pagination

--- src/main/java/pl/sda/final_project/service/ProductService.java
+++ src/main/java/pl/sda/final_project/service/ProductService.java
@@ -1,5 +1,7 @@
 package pl.sda.final_project.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.sda.final_project.dto.ProductDto;
@@ -29,10 +31,13 @@ public class ProductService {
         productRepo.save(productEntityToSave);
     }
 
-    public List<ProductDto> findProducts(Pageable pageable) {
-        return productRepo.findAll(pageable).stream()
+    public Page<ProductDto> findProducts(Pageable pageable) {
+
+        Page<ProductEntity> page = productRepo.findAll(pageable);
+        List<ProductDto> collect = page.stream()
                 .map(ProductDto::apply)
                 .collect(Collectors.toList());
+        return new PageImpl<>(collect, pageable, page.getTotalElements());
     }
 
 
